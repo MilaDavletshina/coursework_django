dfb500ab31d86924951c28a5f0e1155a6bd61c57
ludmila davletshina <ludaplv@mail.ru>
2024-10-07 15:45:05 +0300

    добавлен admin для Client

--- message/admin.py
+++ message/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from message.models import Client
 
-# Register your models here.
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('email', 'name', 'comment')
+    search_fields = ('name',)
+    search_filter = ('name',)
