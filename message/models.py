cde0b5d4c3a7ca68bc2de81b7f48dc5f28428724
ludmila davletshina <ludaplv@mail.ru>
2024-10-07 15:37:50 +0300

    создана модель Client

--- message/models.py
+++ message/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    """Получатель рассылки"""
+    email = models.EmailField(unique=True, verbose_name='e-mail', help_text='введите e-mail')
+    name = models.CharField(max_length=100, verbose_name='ФИО', help_text='укажите ФИО')
+    comment = models.TextField(null=True, blank=True, verbose_name='комментарий', help_text='оставьте комментарий')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Получатель'
+        verbose_name_plural = 'Получатели'
+        ordering = ['name',]
+
+
+# class Sms(models.Model):
+#     """Сообщение"""
