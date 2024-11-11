8bdaab67446b0c4c3aaf0f20d05de41fe613fa39
ludmila davletshina <ludaplv@mail.ru>
2024-11-11 18:16:53 +0300

    login.html удалена кнопка "назад"

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,8 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
-from message.apps import MessageConfig
+from users.apps import UsersConfig
 
-
-app_name = MessageConfig.name
+app_name = UsersConfig.name
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login')
