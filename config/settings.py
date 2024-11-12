6e978b3f60fefba3e4ab12415e8b276783ba0860
ludmila davletshina <ludaplv@mail.ru>
2024-11-12 18:44:40 +0300

    добавлены параметры почты в settings.py

--- config/settings.py
+++ config/settings.py
@@ -100,3 +100,12 @@ AUTH_USER_MODEL = 'users.User' #приложение.Модель
 
 LOGIN_REDIRECT_URL = 'message:main'
 LOGOUT_REDIRECT_URL = 'message:main'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', False) == 'True'
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', False) == 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('MAIL_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
