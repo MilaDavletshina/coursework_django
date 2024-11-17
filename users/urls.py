b630dcbcea7442ada159ca7623e7f111dd92d32e
ludmila davletshina <ludaplv@mail.ru>
2024-11-17 10:18:01 +0300

    путь для password-reset и set-new-password

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from users.apps import UsersConfig
-from users.views import UserCreateView, email_verification
+from users.views import UserCreateView, email_verification, UserForgotPasswordView, UserPasswordResetConfirmView
 
 app_name = UsersConfig.name
 
@@ -9,5 +9,8 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', UserCreateView.as_view(), name='register'),
-    path("email-confirm/<str:token>/", email_verification, name='email-confirm')
+    path("email-confirm/<str:token>/", email_verification, name='email-confirm'),
+
+    path('password-reset/', UserForgotPasswordView.as_view(), name='password_reset'),
+    path('set-new-password/<uidb64>/<token>/', UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
