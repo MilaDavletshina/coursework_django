35d39e86e6d6857fc524473839fa755e275a2b4b
ludmila davletshina <ludaplv@mail.ru>
2024-11-20 11:46:22 +0300

    обновлен views

--- users/views.py
+++ users/views.py
@@ -1,14 +1,16 @@
 import secrets
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.core.exceptions import PermissionDenied
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy, reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from config.settings import EMAIL_HOST_USER
-from users.forms import UserRegisterForm, UserForgotPasswordForm, UserSetNewPasswordForm
+from users.forms import UserRegisterForm, UserForgotPasswordForm, UserSetNewPasswordForm, UserForm, UserUpdateForm
 from users.models import User
 
 
@@ -88,3 +90,50 @@ class UserPasswordResetConfirmView(SuccessMessageMixin, PasswordResetConfirmView
         context = super().get_context_data(**kwargs)
         context["title"] = "Установить новый пароль"
         return context
+
+
+class UserListView(ListView, LoginRequiredMixin):
+    """Пользователь - просмотр"""
+
+    model = User
+    template_name = "users/user_list.html"
+
+
+class UserDetailView(DetailView, LoginRequiredMixin):
+    """Просмотр пользователя"""
+
+    model = User
+    form_class = UserUpdateForm
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        if self.request.user.is_superuser:
+            return self.object
+        raise PermissionDenied
+
+
+class UserUpdateView(UpdateView, LoginRequiredMixin):
+    """Пользователь - обновление"""
+
+    model = User
+    form_class = UserUpdateForm
+    success_url = reverse_lazy("users:user_list")
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        if self.request.user.is_superuser:
+            return self.object
+        raise PermissionDenied
+
+
+class UserDeleteView(DeleteView):
+    """Пользователь - удаление"""
+
+    model = User
+    success_url = reverse_lazy("users:user_list")
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        if self.request.user.is_superuser:
+            return self.object
+        raise PermissionDenied
