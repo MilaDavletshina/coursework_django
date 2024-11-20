8e30a4c0be10b0a028d98798c1d539f038412812
ludmila davletshina <ludaplv@mail.ru>
2024-11-20 10:20:33 +0300

    добавлены права блокировки пользователя, поля имя, фамилия

--- users/models.py
+++ users/models.py
@@ -32,9 +32,13 @@ class User(AbstractUser):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
+    def __str__(self):
+        return self.emai
+
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+        permissions = [
+            ("can_block_user", "can block user"),
+        ]
 
-    def __str__(self):
-        return self.email
